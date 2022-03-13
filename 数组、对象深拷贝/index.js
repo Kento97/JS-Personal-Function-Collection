e4c47925e977da1c1f57d58bad0cae26451ff787
Kento97 <ty.1997@foxmail.com>
2022-03-13 14:58:31 +0800

    深度拷贝

---
+++ 数组、对象深拷贝/index.js
@@ -0,0 +1,37 @@
+const obj = {
+    a: 1,
+    b: NaN,
+    c: {
+        d: [1, 2, 3],
+        e: undefined
+    },
+    d: {
+        f: {
+            g: [1, 2, 3]
+        }
+    }
+}
+const {log} = console
+function checkType(target){
+    return Object.prototype.toString.call(target).slice(8, -1);
+}
+function deepClone(targetData){
+    let type=checkType(targetData);
+    let result;
+    if(type==="Array"){
+        result=[];
+        const arrLen=targetData.length;
+        for (let i = 0; i < arrLen; i++) {
+            result.push(deepClone(targetData[i]));
+        }
+    } else if (type==="Object"){
+        result={};
+        for (let key in targetData) {
+            result[key]=deepClone(targetData[key]);
+        }
+    }else {
+        return targetData;
+    }
+    return result;
+}
+log(deepClone(obj));
