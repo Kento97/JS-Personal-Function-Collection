febdb151cb745b1e9288f379c00a784b882d2615
Human Town <ty.1997@foxmail.com>
2022-03-14 10:21:38 +0800

    将数组中移动0至数组末尾,修改原数组版本

--- 将数组中移动0至数组末尾/index.js
+++ 将数组中移动0至数组末尾/index.js
@@ -1,7 +1,18 @@
-const arr=[0,1,0,2,0,3,0,4];
-let arr2=[]
-arr.reverse().forEach((item) => {
-    item === 0 ? arr2.push(item)
-               : arr2.splice(0,0,item)
-})
-console.log(arr2)
+const arr=[0,1,0,2,0,3,0,4,5,0];
+// let arr2=[]
+// arr.reverse().forEach((item) => {
+//     item === 0 ? arr2.push(item)
+//                : arr2.splice(0,0,item)
+// })
+// console.log(arr2)
+let len=arr.length;
+for (let i = 0; i < len; i++) {
+    console.log(i)
+    if(arr[i]===0){
+        arr.splice(i,1);
+        arr.push(0);
+        i--;
+        len--;
+    }
+}
+console.log(arr)
