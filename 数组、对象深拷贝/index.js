e2afa6b29da67c08052581ba436b38b9818137b2
Human Town <ty.1997@foxmail.com>
2022-03-15 14:27:14 +0800

    weakmap深拷贝

--- 数组、对象深拷贝/index.js
+++ 数组、对象深拷贝/index.js
@@ -14,28 +14,37 @@ const obj = {
     }
 }
 const {log} = console
-function checkType(target){
+
+function checkType(target) {
     return Object.prototype.toString.call(target).slice(8, -1);
 }
-function deepClone(targetData){
-    let type=checkType(targetData);
-    let result;
-    if(type==="Array"){
-        result=[];
-        const arrLen=targetData.length;
-        for (let i = 0; i < arrLen; i++) {
-            result.push(deepClone(targetData[i]));
-        }
-    } else if (type==="Object"){
-        result={};
-        for (let key in targetData) {
-            if (targetData.hasOwnProperty(key)) {
-                result[key]=deepClone(targetData[key]);
+
+function deepClone(data) {
+    const obj = checkType(data) === "Array" ? [] : {};
+    if (['Object', 'Array'].includes(checkType(data))) {
+        for (let key in data) {
+            const value = data[key];
+            //如果拷贝的是简单类型直接进行赋值
+            if (!['Object', 'Array'].includes(checkType(value))) {
+                obj[key] = value;
+            } else {
+                //定义一个弱映射，初始化的时候将data本身加入到映射中
+                const wm = new WeakMap();
+                //如果拷贝的是复杂数据类型，第一次拷贝后存入 WeakMap
+                //第二次再次遇到该值时直接赋值为null，结束递柜
+                wm.set(data, true);
+                if (wm.has(value)) {
+                    obj[key] = null
+                } else {
+                    wm.set(value, true)
+                    obj[key] = deepClone(value);
+                }
             }
         }
-    }else {
-        return targetData;
+    } else {
+        return data;
     }
-    return result;
+    return obj
 }
+
 log(deepClone(obj));
