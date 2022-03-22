33bbf07df9046face0ded6c492c73d1a267191ad
Human Town <ty.1997@foxmail.com>
2022-03-22 23:36:36 +0800

    修改防抖的实现

--- 防抖/index.js
+++ 防抖/index.js
@@ -1,8 +1,12 @@
-function debounce(func, delay) {
-  return function () {
-    setTimeout(() => {
-      func.apply(this, arguments)
-    }, delay)
+function debounce(fn, wait) {
+  let timer = null; //注意点1：借助闭包
+  return function (...args) {
+    if (timer) clearTimeout(timer); //注意点2：清除定时器
+    timer = setTimeout(() => {
+      fn.apply(this, args) //注意点3：setTimout会发生this隐式丢失；改变this指向为调用debounce所指的对象。
+    }, wait)
   }
 }
+
+
 
